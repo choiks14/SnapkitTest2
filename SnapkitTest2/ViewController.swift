976f57aea57a5e14e468c37d09adaae440115489
choiks14 <choiks14@mint-soft.com>
2018-03-29 16:52:24 +0900

    first commit

---
+++ SnapkitTest2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SnapkitTest2
+//
+//  Created by kangsik choi on 2018. 3. 29..
+//  Copyright © 2018 mint-soft. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
